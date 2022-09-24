61827c2b37a6f4e6f1c826883efd9686f4049a80
abdulhamiid <adioabdulhamid@yahoo.com>
2022-09-24 18:14:38 +0100

    Add test to like

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,36 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  subject { Like.new(user_id: 1, post_id: 1) }
-
-  before { subject.save }
-
-  it 'user_id should be present' do
-    subject.user_id = nil
-    expect(subject).to_not be_valid
+  before(:all) do
+    @user = User.create(
+      name: 'Peter',
+      photo: 'https://unsplash.com/box.jpg',
+      bio: 'Engineer',
+      posts_counter: 0
+    )
+    @post = Post.create(
+      # new title
+      title: 'Blog App',
+      text: 'Ruby on Rails',
+      author_id: @user.id,
+      comments_counter: 0,
+      likes_counter: 0
+    )
   end
-
-  it 'post_id should be present' do
-    subject.post_id = nil
-    expect(subject).to_not be_valid
-  end
-
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
-  end
-
-  it 'should belong to user' do
-    assc = described_class.reflect_on_association(:user)
-    expect(assc.macro).to eq :belongs_to
-  end
-
-  it 'should belong to post' do
-    assc = described_class.reflect_on_association(:post)
-    expect(assc.macro).to eq :belongs_to
-  end
-
-  it 'should update post likes_counter' do
-    subject.update_likes_counter
-    expect(subject.post.likes_counter).to eq 1
+  
+  it '@posts like_counter should increase by one' do
+    Like.create(author_id: @user.id, post_id: @post.id)
+    Like.create(author_id: @user.id, post_id: @post.id)
+    post = Post.find(@post.id)
+    expect(post.likes_counter).to eq(2)
   end
 end
