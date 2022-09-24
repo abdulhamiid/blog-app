cbd44760ba7ed61c291944fc3e6ce7dd0de7004c
abdulhamiid <adioabdulhamid@yahoo.com>
2022-09-24 14:07:40 +0100

    Validate user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
+  validates :name, presence: true
+  validates :posts_counter, numericality: true
+
   def recent_posts
     posts.order('created_at Desc').last(3)
   end
