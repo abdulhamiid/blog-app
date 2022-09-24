ad7759bb11705d97f3bab02717c54ab3f1232e5f
abdulhamiid <adioabdulhamid@yahoo.com>
2022-09-24 20:30:58 +0100

    Fix errors

---
+++ spec/comment_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  before(:all) do
+    @user = User.create(
+      name: 'Peter',
+      photo: 'https://unsplash.com/box.jpg',
+      bio: 'Engineer',
+      posts_counter: 0
+    )
+    @post = Post.create(
+      title: 'Blog App',
+      text: 'Ruby on Rails',
+      author_id: @user.id,
+      comments_counter: 0,
+      likes_counter: 0
+    )
+  end
+
+  it '@users comments_counter should increase by one' do
+    Comment.create(text: 'Hello there!!', author_id: @user.id, post_id: @post.id)
+    Comment.create(text: 'Welcome home!!', author_id: @user.id, post_id: @post.id)
+    post = Post.find(@post.id)
+    expect(post.comments_counter).to eq(2)
+  end
+end
