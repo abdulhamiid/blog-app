0c024d6be75632fe2b630e8468fc5c3ebb1d9e0a
abdulhamiid <adioabdulhamid@yahoo.com>
2022-09-29 07:41:33 +0100

    Modify code to handle only the 4 required URLs

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "users#index"
 
-  resources :users do
-    resources :posts, only: [:index, :new, :create]
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
   end
   # Defines the root path route ("/")
   # root "articles#index"
