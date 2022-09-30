29048e9e707b830719cc9ed53fd859d8f4df73ec
abdulhamiid <adioabdulhamid@yahoo.com>
2022-09-30 18:25:01 +0100

    Add post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,10 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @user = User.find(params[:user_id].to_i)
   end
 
   def show
+    @user = User.find(params[:user_id].to_i)
     @post = Post.find(params[:id])
   end
 end
