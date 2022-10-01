55194464a4d96815c33cbaafe7f62c312d8da482
abdulhamiid <adioabdulhamid@yahoo.com>
2022-10-01 11:55:03 +0100

    Add new post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,23 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @article = Post.new
+  end
+
+  def create
+    @post = Post.new(article_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
