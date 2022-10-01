3d4a0319e1a3b09e14742202491b25c80ede869f
abdulhamiid <adioabdulhamid@yahoo.com>
2022-10-01 10:16:04 +0100

    Test users index controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET #index' do
+    before(:each) { get users_path }
+    it 'is success ' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders the correct template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'Contains the placeholder text' do
+      expect(response.body).to match('All Users')
+    end
   end
 end
