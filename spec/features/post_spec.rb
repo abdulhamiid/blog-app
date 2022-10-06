3461e07e041dcb2473fda036d15d8b1c4f6401d5
abdulhamiid <adioabdulhamid@yahoo.com>
2022-10-06 08:26:00 +0100

    Add test to see user profile picture

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "posts/index.html.erb", type: :feature do
   before :each do
     # create use
-    User.create(name: 'John Doe', bio: 'Lorem ipsum dolor sit user\'s bio', posts_counter: 0)
+    User.create(name: 'John Doe', bio: 'Lorem ipsum dolor sit user\'s bio', photo: 'https://www.w3schools.com/howto/img_avatar.png', posts_counter: 0)
     
     # create posts
     Post.create(title: 'Capybara', text: 'Capybara helps you test web applications', author_id: User.first.id, comments_counter: 0, likes_counter: 0)
@@ -15,29 +15,29 @@ RSpec.describe "posts/index.html.erb", type: :feature do
     visit user_posts_path(User.first)
   end
 
+  it "displays user photo" do
+    expect(page).to have_css("img[src*='https://www.w3schools.com/howto/img_avatar.png']")
+  end
+
   it "display user's username" do
     expect(page).to have_text('John Doe')
   end
 
   it "has the number of posts the user has written" do
-    expect(page).to have_text 'Capybara'
-    expect(page).to have_text 'Title 2'
-    expect(page).to have_text 'Title 3'
+    expect(page).to have_text('Number of posts: 3')
+  end
+
+  it "has a post title" do
+    expect(page).to have_text('Capybara')
   end
 
   it "has first comments on a post" do
     expect(page).to have_text 'Username: This is the fist comment on capybara'
   end
 
-  it "has the posts text" do
-    expect(page).to have_text 'Capybara helps you test web applications'
-    expect(page).to have_text 'Lorem ipsum dolor sit'
-  end
-
-  it "has the number of comments and likes for each post" do
+  it "has the number of comments and likes for a post" do
     expect(page).to have_text 'Comments: 1'
     expect(page).to have_text 'Comments: 0'
-    expect(page).to have_text 'Likes: 0'
   end
 
   it "has pagination button section" do
