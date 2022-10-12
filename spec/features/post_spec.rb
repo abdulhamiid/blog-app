d2d5702a23ad5e01385ded77297a2d0338cf19d7
abdulhamiid <adioabdulhamid@yahoo.com>
2022-10-12 19:04:33 +0100

    Fix linters errors

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -19,98 +19,94 @@ RSpec.describe 'posts', type: :feature do
   end
 
   # User post index page tests
-  context 'posts/index.html.erb' do
-    before :each do
-      visit user_posts_path(User.first)
-    end
+  before :each do
+    visit user_posts_path(User.first)
+  end
 
-    it 'displays user photo' do
-      expect(page).to have_css("img[src*='https://www.w3schools.com/howto/img_avatar.png']")
-    end
+  it 'displays user photo' do
+    expect(page).to have_css("img[src*='https://www.w3schools.com/howto/img_avatar.png']")
+  end
 
-    it "display user's username" do
-      expect(page).to have_text('John Doe')
-    end
+  it "display user's username" do
+    expect(page).to have_text('John Doe')
+  end
 
-    it 'has the number of posts the user has written' do
-      expect(page).to have_text('Number of posts: 3')
-    end
+  it 'has the number of posts the user has written' do
+    expect(page).to have_text('Number of posts: 3')
+  end
 
-    it 'has a post title' do
-      expect(page).to have_text('Capybara')
-    end
+  it 'has a post title' do
+    expect(page).to have_text('Capybara')
+  end
 
-    it 'has first comments on a post' do
-      expect(page).to have_text 'John Doe: This is the fist comment on capybara'
-    end
+  it 'has first comments on a post' do
+    expect(page).to have_text 'John Doe: This is the fist comment on capybara'
+  end
 
-    it 'has the number of comments and likes for a post' do
-      expect(page).to have_text 'Comments: 1'
-      expect(page).to have_text 'Comments: 0'
-    end
+  it 'has the number of comments and likes for a post' do
+    expect(page).to have_text 'Comments: 1'
+    expect(page).to have_text 'Comments: 0'
+  end
 
-    it 'has pagination button section' do
-      expect(page).to have_button 'Pagination'
-    end
+  it 'has pagination button section' do
+    expect(page).to have_button 'Pagination'
+  end
 
-    it "button click redirect to user's posts index page" do
-      expect(page).to have_link 'Capybara'
-      click_link 'Capybara'
-      expect(page).to have_current_path(user_post_path(User.first, Post.first))
-    end
+  it "button click redirect to user's posts index page" do
+    expect(page).to have_link 'Capybara'
+    click_link 'Capybara'
+    expect(page).to have_current_path(user_post_path(User.first, Post.first))
+  end
 
-    it 'has a link to new post page' do
-      expect(page).to have_link 'New Post'
-      click_link 'New Post'
-      expect(page).to have_current_path(new_user_post_path(User.first))
-    end
+  it 'has a link to new post page' do
+    expect(page).to have_link 'New Post'
+    click_link 'New Post'
+    expect(page).to have_current_path(new_user_post_path(User.first))
   end
 
   # User post show page tests
-  context 'posts/show.html.erb' do
-    before :each do
-      visit user_post_path(User.first, Post.first)
-    end
+  before :each do
+    visit user_post_path(User.first, Post.first)
+  end
 
-    it 'has the post title' do
-      expect(page).to have_text('Capybara')
-    end
+  it 'has the post title' do
+    expect(page).to have_text('Capybara')
+  end
 
-    it 'has the post author' do
-      expect(page).to have_text('Post by John Doe')
-    end
+  it 'has the post author' do
+    expect(page).to have_text('Post by John Doe')
+  end
 
-    it 'has the post number of comments' do
-      expect(page).to have_text('Comments: 1')
-    end
+  it 'has the post number of comments' do
+    expect(page).to have_text('Comments: 1')
+  end
 
-    it 'has the post number of likes' do
-      expect(page).to have_text('Likes: 0')
-    end
+  it 'has the post number of likes' do
+    expect(page).to have_text('Likes: 0')
+  end
 
-    it 'has the post body' do
-      expect(page).to have_text('Capybara helps you test web applications')
-    end
+  it 'has the post body' do
+    expect(page).to have_text('Capybara helps you test web applications')
+  end
 
-    it 'has the username of each commenter' do
-      expect(page).to have_text('John Doe:')
-    end
+  it 'has the username of each commenter' do
+    expect(page).to have_text('John Doe:')
+  end
 
-    it 'has the username of each commenter' do
-      expect(page).to have_text('John Doe: This is the fist comment on capybara')
-    end
+  it 'has the username of each commenter' do
+    expect(page).to have_text('John Doe: This is the fist comment on capybara')
+  end
 
-    it 'has a link to add a new comment' do
-      expect(page).to have_link('Add a new comment')
-    end
+  it 'has a link to add a new comment' do
+    expect(page).to have_link('Add a new comment')
+  end
 
-    it 'add a new comment link redirect to new comment page' do
-      click_link('Add a new comment')
-      expect(page).to have_current_path(new_user_post_comment_path(User.first, Post.first))
-    end
+  it 'add a new comment link redirect to new comment page' do
+    click_link('Add a new comment')
+    expect(page).to have_current_path(new_user_post_comment_path(User.first, Post.first))
+  end
 
-    it 'has like post button' do
-      expect(page).to have_button('Like post')
-    end
+  it 'has like post button' do
+    expect(page).to have_button('Like post')
   end
 end
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,96 +1,92 @@
 require 'rails_helper'
 
 RSpec.describe 'users', type: :feature do
-  context 'users/index.html.erb page' do
-    before :each do
-      User.create(name: 'John Doe', bio: 'Lorem ipsum dolor sit', posts_counter: 0)
-      User.create(name: 'Mary Williams', bio: 'Lorem ipsum dolor', posts_counter: 0)
-    end
-    it 'displays page header' do
-      visit users_path
-      expect(page).to have_text('All Users')
-    end
-
-    it 'displays all users' do
-      visit users_path
-      expect(page).to have_content 'John Doe'
-      expect(page).to have_content 'Mary Williams'
-    end
-
-    it 'displays number of posts for each user' do
-      visit users_path
-      expect(page).to have_content 'Number of posts: 0'
-    end
-
-    it 'it redirect to user show page' do
-      visit users_path
-      expect(page).to have_link 'John Doe', href: user_path(User.first)
-      click_link 'John Doe'
-      expect(page).to have_current_path(user_path(User.first))
-    end
-
-    it 'displays correct user name by clicking on user' do
-      visit users_path
-      click_on 'Mary Williams'
-      expect(page).to have_content 'Mary Williams'
-      expect(page).to have_content 'Lorem ipsum dolor'
-    end
+  before :each do
+    User.create(name: 'John Doe', bio: 'Lorem ipsum dolor sit', posts_counter: 0)
+    User.create(name: 'Mary Williams', bio: 'Lorem ipsum dolor', posts_counter: 0)
+  end
+  it 'displays page header' do
+    visit users_path
+    expect(page).to have_text('All Users')
   end
 
-  context 'users/show.html.erb' do
-    before :each do
-      # create user
-      User.create(name: 'John Doe', bio: 'Lorem ipsum dolor sit user\'s bio', posts_counter: 0)
+  it 'displays all users' do
+    visit users_path
+    expect(page).to have_content 'John Doe'
+    expect(page).to have_content 'Mary Williams'
+  end
 
-      # create posts
-      Post.create(title: 'Title 1', text: 'Lorem ipsum dolor sit', author_id: User.first.id, comments_counter: 0,
-                  likes_counter: 0)
-      Post.create(title: 'Title 2', text: 'Lorem ipsum dolor sit', author_id: User.first.id, comments_counter: 0,
-                  likes_counter: 0)
-      Post.create(title: 'Title 3', text: 'Lorem ipsum dolor sit', author_id: User.first.id, comments_counter: 0,
-                  likes_counter: 0)
-      Post.create(title: 'Title 4', text: 'Lorem ipsum dolor sit', author_id: User.first.id, comments_counter: 0,
-                  likes_counter: 0)
-      Post.create(title: 'Title 5', text: 'Lorem ipsum dolor sit', author_id: User.first.id, comments_counter: 0,
-                  likes_counter: 0)
+  it 'displays number of posts for each user' do
+    visit users_path
+    expect(page).to have_content 'Number of posts: 0'
+  end
 
-      visit user_url(User.first)
-    end
+  it 'it redirect to user show page' do
+    visit users_path
+    expect(page).to have_link 'John Doe', href: user_path(User.first)
+    click_link 'John Doe'
+    expect(page).to have_current_path(user_path(User.first))
+  end
 
-    it "display user's username" do
-      expect(page).to have_text('John Doe')
-    end
+  it 'displays correct user name by clicking on user' do
+    visit users_path
+    click_on 'Mary Williams'
+    expect(page).to have_content 'Mary Williams'
+    expect(page).to have_content 'Lorem ipsum dolor'
+  end
 
-    it 'has the number of posts the user has written' do
-      expect(page).to have_text('Number of posts: 5')
-    end
+  before :each do
+    # create user
+    User.create(name: 'John Doe', bio: 'Lorem ipsum dolor sit user\'s bio', posts_counter: 0)
 
-    it "displays user's bio" do
-      expect(page).to have_content 'Lorem ipsum dolor sit user\'s bio'
-    end
+    # create posts
+    Post.create(title: 'Title 1', text: 'Lorem ipsum dolor sit', author_id: User.first.id, comments_counter: 0,
+                likes_counter: 0)
+    Post.create(title: 'Title 2', text: 'Lorem ipsum dolor sit', author_id: User.first.id, comments_counter: 0,
+                likes_counter: 0)
+    Post.create(title: 'Title 3', text: 'Lorem ipsum dolor sit', author_id: User.first.id, comments_counter: 0,
+                likes_counter: 0)
+    Post.create(title: 'Title 4', text: 'Lorem ipsum dolor sit', author_id: User.first.id, comments_counter: 0,
+                likes_counter: 0)
+    Post.create(title: 'Title 5', text: 'Lorem ipsum dolor sit', author_id: User.first.id, comments_counter: 0,
+                likes_counter: 0)
 
-    it "displays user's first 3 posts" do
-      expect(page).to have_content 'Title 1'
-      expect(page).to have_content 'Title 2'
-      expect(page).to have_content 'Title 3'
-      expect(page).not_to have_content 'Title 4'
-      expect(page).not_to have_content 'Title 5'
-    end
+    visit user_url(User.first)
+  end
 
-    it 'has button to see all posts' do
-      expect(page).to have_button 'See all posts'
-      expect(page).to have_link 'See all post', href: user_posts_url(User.first)
-    end
+  it "display user's username" do
+    expect(page).to have_text('John Doe')
+  end
 
-    it "button click redirect to user's posts index page" do
-      click_link 'See all posts'
-      expect(page).to have_current_path(user_posts_path(User.first))
-      expect(page).to have_link 'New Post', href: new_user_post_path(User.first)
-      expect(page).to have_content 'Title 1'
-      expect(page).to have_content 'Title 2'
-      expect(page).to have_content 'Title 3'
-      expect(page).to have_content 'Title 4'
-      expect(page).to have_content 'Title 5'
-    end
+  it 'has the number of posts the user has written' do
+    expect(page).to have_text('Number of posts: 5')
+  end
+
+  it "displays user's bio" do
+    expect(page).to have_content 'Lorem ipsum dolor sit user\'s bio'
+  end
+
+  it "displays user's first 3 posts" do
+    expect(page).to have_content 'Title 1'
+    expect(page).to have_content 'Title 2'
+    expect(page).to have_content 'Title 3'
+    expect(page).not_to have_content 'Title 4'
+    expect(page).not_to have_content 'Title 5'
+  end
+
+  it 'has button to see all posts' do
+    expect(page).to have_button 'See all posts'
+    expect(page).to have_link 'See all post', href: user_posts_url(User.first)
+  end
+
+  it "button click redirect to user's posts index page" do
+    click_link 'See all posts'
+    expect(page).to have_current_path(user_posts_path(User.first))
+    expect(page).to have_link 'New Post', href: new_user_post_path(User.first)
+    expect(page).to have_content 'Title 1'
+    expect(page).to have_content 'Title 2'
+    expect(page).to have_content 'Title 3'
+    expect(page).to have_content 'Title 4'
+    expect(page).to have_content 'Title 5'
   end
 end
