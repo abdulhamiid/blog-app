a2c7e5234b80eb6d808d9faea5e27d30d8ac47d7
abdulhamiid <adioabdulhamid@yahoo.com>
2022-10-12 17:13:54 +0100

    Fix linters errors

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,26 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe "posts", type: :feature do
+RSpec.describe 'posts', type: :feature do
   before :each do
     # create use
-    User.create(name: 'John Doe', bio: 'Lorem ipsum dolor sit user\'s bio', photo: 'https://www.w3schools.com/howto/img_avatar.png', posts_counter: 0)
-    
+    User.create(name: 'John Doe', bio: 'Lorem ipsum dolor sit user\'s bio',
+                photo: 'https://www.w3schools.com/howto/img_avatar.png', posts_counter: 0)
+
     # create posts
-    Post.create(title: 'Capybara', text: 'Capybara helps you test web applications', author_id: User.first.id, comments_counter: 0, likes_counter: 0)
-    Post.create(title: 'Title 2', text: 'Lorem ipsum dolor sit', author_id: User.first.id, comments_counter: 0, likes_counter: 0)
-    Post.create(title: 'Title 3', text: 'Lorem ipsum dolor sit', author_id: User.first.id, comments_counter: 0, likes_counter: 0)
+    Post.create(title: 'Capybara', text: 'Capybara helps you test web applications', author_id: User.first.id,
+                comments_counter: 0, likes_counter: 0)
+    Post.create(title: 'Title 2', text: 'Lorem ipsum dolor sit', author_id: User.first.id, comments_counter: 0,
+                likes_counter: 0)
+    Post.create(title: 'Title 3', text: 'Lorem ipsum dolor sit', author_id: User.first.id, comments_counter: 0,
+                likes_counter: 0)
 
     # create comments
     Comment.create(text: 'This is the fist comment on capybara', post_id: Post.first.id, author_id: User.first.id)
   end
 
   # User post index page tests
-  context "posts/index.html.erb" do
+  context 'posts/index.html.erb' do
     before :each do
       visit user_posts_path(User.first)
     end
 
-    it "displays user photo" do
+    it 'displays user photo' do
       expect(page).to have_css("img[src*='https://www.w3schools.com/howto/img_avatar.png']")
     end
 
@@ -28,24 +32,24 @@ RSpec.describe "posts", type: :feature do
       expect(page).to have_text('John Doe')
     end
 
-    it "has the number of posts the user has written" do
+    it 'has the number of posts the user has written' do
       expect(page).to have_text('Number of posts: 3')
     end
 
-    it "has a post title" do
+    it 'has a post title' do
       expect(page).to have_text('Capybara')
     end
 
-    it "has first comments on a post" do
+    it 'has first comments on a post' do
       expect(page).to have_text 'John Doe: This is the fist comment on capybara'
     end
 
-    it "has the number of comments and likes for a post" do
+    it 'has the number of comments and likes for a post' do
       expect(page).to have_text 'Comments: 1'
       expect(page).to have_text 'Comments: 0'
     end
 
-    it "has pagination button section" do
+    it 'has pagination button section' do
       expect(page).to have_button 'Pagination'
     end
 
@@ -55,7 +59,7 @@ RSpec.describe "posts", type: :feature do
       expect(page).to have_current_path(user_post_path(User.first, Post.first))
     end
 
-    it "has a link to new post page" do
+    it 'has a link to new post page' do
       expect(page).to have_link 'New Post'
       click_link 'New Post'
       expect(page).to have_current_path(new_user_post_path(User.first))
@@ -63,49 +67,49 @@ RSpec.describe "posts", type: :feature do
   end
 
   # User post show page tests
-  context "posts/show.html.erb" do
+  context 'posts/show.html.erb' do
     before :each do
       visit user_post_path(User.first, Post.first)
     end
 
-    it "has the post title" do
+    it 'has the post title' do
       expect(page).to have_text('Capybara')
     end
 
-    it "has the post author" do
+    it 'has the post author' do
       expect(page).to have_text('Post by John Doe')
     end
 
-    it "has the post number of comments" do
+    it 'has the post number of comments' do
       expect(page).to have_text('Comments: 1')
     end
 
-    it "has the post number of likes" do
+    it 'has the post number of likes' do
       expect(page).to have_text('Likes: 0')
     end
 
-    it "has the post body" do
+    it 'has the post body' do
       expect(page).to have_text('Capybara helps you test web applications')
     end
 
-    it "has the username of each commenter" do
+    it 'has the username of each commenter' do
       expect(page).to have_text('John Doe:')
     end
 
-    it "has the username of each commenter" do
+    it 'has the username of each commenter' do
       expect(page).to have_text('John Doe: This is the fist comment on capybara')
     end
 
-    it "has a link to add a new comment" do
+    it 'has a link to add a new comment' do
       expect(page).to have_link('Add a new comment')
     end
 
-    it "add a new comment link redirect to new comment page" do
+    it 'add a new comment link redirect to new comment page' do
       click_link('Add a new comment')
       expect(page).to have_current_path(new_user_post_comment_path(User.first, Post.first))
     end
 
-    it "has like post button" do
+    it 'has like post button' do
       expect(page).to have_button('Like post')
     end
   end
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,35 +1,35 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "users", type: :feature do
-  context "users/index.html.erb page" do
+RSpec.describe 'users', type: :feature do
+  context 'users/index.html.erb page' do
     before :each do
       User.create(name: 'John Doe', bio: 'Lorem ipsum dolor sit', posts_counter: 0)
       User.create(name: 'Mary Williams', bio: 'Lorem ipsum dolor', posts_counter: 0)
     end
-    it "displays page header" do
+    it 'displays page header' do
       visit users_path
       expect(page).to have_text('All Users')
     end
 
-    it "displays all users" do
+    it 'displays all users' do
       visit users_path
       expect(page).to have_content 'John Doe'
       expect(page).to have_content 'Mary Williams'
     end
 
-    it "displays number of posts for each user" do
+    it 'displays number of posts for each user' do
       visit users_path
       expect(page).to have_content 'Number of posts: 0'
     end
 
-    it "it redirect to user show page" do
+    it 'it redirect to user show page' do
       visit users_path
       expect(page).to have_link 'John Doe', href: user_path(User.first)
       click_link 'John Doe'
       expect(page).to have_current_path(user_path(User.first))
     end
 
-    it "displays correct user name by clicking on user" do
+    it 'displays correct user name by clicking on user' do
       visit users_path
       click_on 'Mary Williams'
       expect(page).to have_content 'Mary Williams'
@@ -37,34 +37,38 @@ RSpec.describe "users", type: :feature do
     end
   end
 
-  context "users/show.html.erb" do
+  context 'users/show.html.erb' do
     before :each do
       # create user
       User.create(name: 'John Doe', bio: 'Lorem ipsum dolor sit user\'s bio', posts_counter: 0)
-      
+
       # create posts
-      Post.create(title: 'Title 1', text: 'Lorem ipsum dolor sit', author_id: User.first.id, comments_counter: 0, likes_counter: 0)
-      Post.create(title: 'Title 2', text: 'Lorem ipsum dolor sit', author_id: User.first.id, comments_counter: 0, likes_counter: 0)
-      Post.create(title: 'Title 3', text: 'Lorem ipsum dolor sit', author_id: User.first.id, comments_counter: 0, likes_counter: 0)
-      Post.create(title: 'Title 4', text: 'Lorem ipsum dolor sit', author_id: User.first.id, comments_counter: 0, likes_counter: 0)
-      Post.create(title: 'Title 5', text: 'Lorem ipsum dolor sit', author_id: User.first.id, comments_counter: 0, likes_counter: 0)
-  
+      Post.create(title: 'Title 1', text: 'Lorem ipsum dolor sit', author_id: User.first.id, comments_counter: 0,
+                  likes_counter: 0)
+      Post.create(title: 'Title 2', text: 'Lorem ipsum dolor sit', author_id: User.first.id, comments_counter: 0,
+                  likes_counter: 0)
+      Post.create(title: 'Title 3', text: 'Lorem ipsum dolor sit', author_id: User.first.id, comments_counter: 0,
+                  likes_counter: 0)
+      Post.create(title: 'Title 4', text: 'Lorem ipsum dolor sit', author_id: User.first.id, comments_counter: 0,
+                  likes_counter: 0)
+      Post.create(title: 'Title 5', text: 'Lorem ipsum dolor sit', author_id: User.first.id, comments_counter: 0,
+                  likes_counter: 0)
+
       visit user_url(User.first)
     end
-  
+
     it "display user's username" do
       expect(page).to have_text('John Doe')
     end
-  
-    it "has the number of posts the user has written" do
+
+    it 'has the number of posts the user has written' do
       expect(page).to have_text('Number of posts: 5')
     end
-  
-  
+
     it "displays user's bio" do
       expect(page).to have_content 'Lorem ipsum dolor sit user\'s bio'
     end
-  
+
     it "displays user's first 3 posts" do
       expect(page).to have_content 'Title 1'
       expect(page).to have_content 'Title 2'
@@ -72,12 +76,12 @@ RSpec.describe "users", type: :feature do
       expect(page).not_to have_content 'Title 4'
       expect(page).not_to have_content 'Title 5'
     end
-  
-    it "has button to see all posts" do
+
+    it 'has button to see all posts' do
       expect(page).to have_button 'See all posts'
       expect(page).to have_link 'See all post', href: user_posts_url(User.first)
     end
-  
+
     it "button click redirect to user's posts index page" do
       click_link 'See all posts'
       expect(page).to have_current_path(user_posts_path(User.first))
