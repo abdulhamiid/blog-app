b99005d03a86aafa983034afedd95d9ec811681d
abdulhamiid <adioabdulhamid@yahoo.com>
2022-10-15 17:37:30 +0100

    Fix linters errors

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,27 +1,31 @@
 module Api
   module V1
-    class Api::V1::CommentsController < AuthenticationController 
-      def index
-        @user = User.includes(posts: [:comments]).find(params[:user_id])
-        @post = @user.posts.find_by(id: params[:post_id])
-        @comment = @post.comments
-        render json: @comment
-      end
-    
-      def new
-        @comment = Comment.new
-      end
+    module Api
+      module V1
+        class CommentsController < AuthenticationController
+          def index
+            @user = User.includes(posts: [:comments]).find(params[:user_id])
+            @post = @user.posts.find_by(id: params[:post_id])
+            @comment = @post.comments
+            render json: @comment
+          end
 
-      def create
-        @post = Post.find(params[:post_id])
-        @comment = current_user.comments.new(post_id: @post.id, author_id: current_user.id, text: comment_params)
-        @comment.post_id = @post.id
-        
-        render json: @comment if @comment.save
-      end
+          def new
+            @comment = Comment.new
+          end
 
-      def comment_params
-        params.require(:comment).permit(:text)[:text]
+          def create
+            @post = Post.find(params[:post_id])
+            @comment = current_user.comments.new(post_id: @post.id, author_id: current_user.id, text: comment_params)
+            @comment.post_id = @post.id
+
+            render json: @comment if @comment.save
+          end
+
+          def comment_params
+            params.require(:comment).permit(:text)[:text]
+          end
+        end
       end
     end
   end
