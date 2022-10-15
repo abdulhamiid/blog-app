b3c16def65c92bb3635421abf46f4180cb92ed9b
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-10-15 17:10:31 +0100

    Add authentication controller

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class AuthenticationController < ApiController
-      # rescue_from ActionController::ParameterMissing, with: :parameter_missing
+      rescue_from ActionController::ParameterMissing, with: :parameter_missing
       before_action :authorize_request, except: [:create]
       def create
         user = User.find_by(name: params[:name])
